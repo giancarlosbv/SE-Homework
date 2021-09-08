d1b48040a51e0375b119febdfa4f6248a7ca4e05
Giancarlos Blanco <giancarlosbv@gmail.com>
2021-09-08 12:20:37 -0400

    first commit

--- Jeopardy Lab/jeopardy/src/App.js
+++ Jeopardy Lab/jeopardy/src/App.js
@@ -1,44 +1,111 @@
 import './App.css';
 import { Component } from 'react';
-import { render } from 'react-dom';
-class Header extends Component{
-  render(){
-    return (
-      <div>
+import React from 'react';
 
-      </div>
-    )
-  }
-}
 
-class RandomButton extends Component{
-  render(){
-    return(
-      <div> Hello World
 
-      </div>
-    )
-  }
+// Make another div in your HTML where you display the answer
 
-  JeopardyQuestions(params) {
-    fetch("http://jservice.io/api/random?count=10")
-        .then((response) => response.json())
-        .then(data => console.log(data))
-        // .then((data) => displayAnime(data))
-        .catch((error) => console.log(error))
-    return "done"
-  }
 
-}
+
+// class Header extends Component{
+//   render(){
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   }
+// }
+
+// class RandomButton extends Component{
+//   render(){
+//     return(
+//       <div> Hello World
+
+//       </div>
+//     )
+//   }
+
+
+// On click, have this button that will make a GET request (using fetch) to get random trivia data. Let's start you off with a win by giving you the URL: http://jservice.io/api/random
+ 
+
+
+
 class App extends Component{
+  state = {
+    questions:{}
+  }
+
+
+ JeopardyQuestions=() =>{
+    
+    fetch("http://jservice.io/api/random?count=1")
+        .then((response) => response.json())
+        .then(data=> (data))
+        .then(data => this.setState({questions:data[0]}))
+        .catch((error) => console.log(error))
+        
+    return "done"
+    
+  }
+
+
+
+displayQuestions =(data)=>{
+//destructuring objects
+   
+    // console.log(question)
+    // console.log(category.title)
+    // console.log(`Points: ${value}`)
+    
+    // let question = data[0].question
+    // let category = data[0].category.title
+    // let points = `${data[0].value} points`
+   
+  }
+  
+  // Make a button labeled "Random Trivia Question" or "Get Question" or something descriptive.
   render(){
+    console.log(this.state)
+    let {question,category,value,answer} = this.state.questions
+    // console.log(question,value)
+    if(category){
+      console.log(category.title)
+    }
     return(
       <div>
-
+      <button onClick={()=>this.JeopardyQuestions()}>Get Started</button>
+      {this.state.questions&&category&&<div>{`Question: ${question}`}<br/><br/><br/><br/></div>}
+      {this.state.questions&&category&&<div>{`Category: ${category.title}`}<br/><br/><br/><br/></div>}
+      {this.state.questions&&category&&<div>{`Points: ${category.clues_count}`}<br/><br/><br/><br/></div>}
+      {this.state.questions&&category&&<div>{`Answer: ${answer}`}<br/><br/><br/><br/></div>}
+      
+      
+      
       </div>
     )
   }
+  
 }
-console.log(JeopardyQuestions)
+
+
+
+class Question extends Component{
+
+  // Make some html elements like a div where you will display the question, category and points. NOTE: You will need to look at the data that is returned to you and think back about how to access data from different datatypes (is it an array within an object? How do you access the info?).
+
+
+  //key value pairs, question, points, category.title
+  render(){
+    return(
+      <div><ul>
+
+      </ul></div>
+    )
+  }
+}
+// console.log(JeopardyQuestions())
 
 export default App;
